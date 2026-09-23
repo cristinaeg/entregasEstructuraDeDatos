97b8f7fc05be23a91937a10f0942b58b7af0f03f
sebastiansalazar166199-cpu <sebastiansalazar166199@correo.itm.edu.co>
2026-09-22 19:06:14 -0500

    Add Carta class with constructors and methods

--- carta.cpp
+++ carta.cpp
@@ -2,6 +2,21 @@
 #include <iostream>
 using namespace std;
 
+class Carta {
+private:
+    int numero;
+    string color;
+
+public:
+    Carta();                      // carta vacia
+    Carta(int num, string col);   // carta con datos
+
+    int getNumero();
+    string getColor();
+    bool esValida();
+    void mostrar();
+};
+
 Carta::Carta() {
     numero = -1;
     color = "NINGUNA";
